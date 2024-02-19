5a4568b84efe11b68a5b28413be4f7414d749ecb
jiankimr <jiankimr@gmail.com>
2024-02-20 01:12:11 +0900

    ver2

--- src/main/frontend/react/src/setProxy.js
+++ src/main/frontend/react/src/setProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://52.79.230.93:8080',
       changeOrigin: true,
     })
   );
