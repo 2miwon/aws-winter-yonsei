c5b58c5964952940c06479e95242252fb6767fa9
2miwon <zhengsfsf@gmail.com>
2024-02-23 00:59:52 +0900

    refactor keword

--- src/main/frontend/react/src/api/search.js
+++ src/main/frontend/react/src/api/search.js
@@ -12,14 +12,29 @@ export const fetchSearchResults = async (query, page, sort) => {
 
 
 
-export const fetchKeword = async (query) => {
-  // 추천 검색어 요청해서 받아오기\
-    try {
-      //const response = await axios.get(`http://52.78.206.96:5000/keword/${query}`);
-        const response = await axios.post(`/getKeyword`, { query: query });
-        return (response.data.result[0].items).concat(response.data.result[1].items);
-    } catch (error) {
-      throw new Error('Failed to fetch keword results');
-    }
-};
+  export const fetchKeword = async (query) => {  
+    // 추천 검색어 요청해서 받아오기\
+      try {
+        const response = await axios.post('https://likms.assembly.go.kr/nsrch/ark/ark_trans.do', {
+          convert: 'fw',
+          target: 'common',
+          charset: 'utf-8',
+          query: query,
+          datatype: 'json',
+        });
+  
+        const keywords = [];
+        for (const result of response.data.result) {
+          if (result.totalcount) {
+            for (const item of result.items) {
+              keywords.push(item.keyword);
+            }
+          }
+        }      
+  
+        return keywords;
+      } catch (error) {
+        throw new Error('Failed to fetch keword results');
+      }
+  };
 
