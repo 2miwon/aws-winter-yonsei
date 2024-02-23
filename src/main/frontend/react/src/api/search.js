d2e3e77b12c0b43c122c8c8a21ce3ce39220c068
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2024-02-23 13:28:56 +0900

    자동완성 구현 중

--- src/main/frontend/react/src/api/search.js
+++ src/main/frontend/react/src/api/search.js
@@ -22,24 +22,36 @@ export const fetchSearchResults = async (query, page, sort) => {
 
 
 
-  export const fetchKeword = async (query) => {  
-    // 추천 검색어 요청해서 받아오기\
-      try {
-        const response = await axios.post('https://likms.assembly.go.kr/nsrch/ark/ark_trans.do', {
-          convert: 'fw',
-          target: 'common',
-          charset: 'utf-8',
-          query: query,
-          datatype: 'json',
-        }, {
-          headers: {
-            'User-Agent': 'www.allaw.site'
-          }
+export const fetchKeword = async (query) => {
+    try {
+        const response = await axios.get('https://ysu-004.es.us-east-2.aws.elastic-cloud.com/autocomplete/_search', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'ApiKey STh4TDA0MEJ3enFyUy1PS1RuRXI6WFZNVUpid0JUVi1GdTRVV21QSFdzZw=='
+            },
+            params: {
+                q: `query: "${query}"`
+            }
+        });
+        // 검색 결과를 서버로 전달하여 HTML 형식으로 렌더링된 페이지를 받음
+        const searchResultHtml = await axios.post('http://52.78.206.96:5000/render-search-results', {
+            searchResults: response.data
         });
 
-        return (response.data.result[0].items).concat(response.data.result[1].items);
-      } catch (error) {
-        throw new Error('Failed to fetch keword results');
-      }
-  };
+        // 새 창 열어서 검색 결과 출력
+        let windowObj = window.open('', 'Print', 'width=1350, height=800, toolbars=no, scrollbars=no, status=no, resizable=no');
+        windowObj.document.write(searchResultHtml.data);
+        windowObj.document.close();
+        windowObj.focus();
+        windowObj.print();
+        // 응답 데이터 처리
+        const hits = response.data.hits.hits;
+        const keywords = hits.map(hit => hit._source.search_field);
+
+        return keywords;
+    } catch (error) {
+        console.error('Failed to fetch keyword results:', error);
+        throw new Error('Failed to fetch keyword results');
+    }
+};
 
--- src/main/java/com/yonsei/demo/controller/KeywordController.java
+++ src/main/java/com/yonsei/demo/controller/KeywordController.java
@@ -1,27 +1,22 @@
 package com.yonsei.demo.controller;
 
+import com.yonsei.demo.service.ElasticsearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequiredArgsConstructor
 public class KeywordController {
 
+    private final ElasticsearchService elasticsearchService;
+
     @PostMapping("/getKeyword")
-    public ResponseEntity<String> getKeyword(@RequestBody String query) {
-        String url = "https://likms.assembly.go.kr/nsrch/ark/ark_trans.do";
-        String data = "{\"convert\":\"fw\",\"target\":\"common\",\"charset\":\"utf-8\",\"query\":\"" + query + "\",\"datatype\":\"json\"}";
-
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.postForObject(url, data, String.class);
-
-        if (response != null) {
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Failed to retrieve keywords", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<String> getKeyword(@RequestBody String requestJson) {
+        String response = elasticsearchService.searchAutocomplete(requestJson);
+        return ResponseEntity.ok(response);
     }
 }
