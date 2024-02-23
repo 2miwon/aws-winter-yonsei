123a16cc4095ed06f6efb4fe13e65c7c5560c5ae
jongheon <heon0128@gmail.com>
2024-02-23 12:33:14 +0900

    검색 url 수정

--- src/main/frontend/react/src/api/search.js
+++ src/main/frontend/react/src/api/search.js
@@ -2,11 +2,21 @@ import { queryAllByAltText } from '@testing-library/react';
 import axios from 'axios';
 
 export const fetchSearchResults = async (query, page, sort) => {
+    const BACKEND_URL = `http://52.78.206.96:8000/search/${query}?page=${page}&sort=${sort}`;
+    const BACKEND_URL_2 = `https://allaw.site:8000/search/${query}?page=${page}&sort=${sort}`;
     try {
-      const response = await axios.get(`http://52.78.206.96:5000/search/${query}?page=${page}&sort=${sort}`);
+        const response = await axios.get(BACKEND_URL);
+        console.log(response);
       return response.data;
     } catch (error) {
-      throw new Error('Failed to fetch search results');
+        try {
+            const response = await axios.get(BACKEND_URL_2);
+            return response.data; // 백엔드에서 반환한 응답 (BACKEND_URL_2 사용)
+        } catch (error) {
+            console.error('Error sending message with BACKEND_URL_2:', error);
+            // BACKEND_URL_2에서도 오류가 발생한 경우, 오류 처리
+            return {error: 'Failed to send message with both URLs'}; // 최종 오류 처리
+        }
     }
   };
 
