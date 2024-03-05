134832fd734b01b2a895e21d81438e57ce9fdae8
2miwon <zhengsfsf@gmail.com>
2024-03-06 06:31:44 +0900

    bash refactor

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
-
-docker build -t 2miwon/allaw-web .
-docker push 2miwon/allaw-web
+git reset --hard origin/deploy ;
+git pull ;
+docker build -t 2miwon/allaw-web . ;
+docker push 2miwon/allaw-web ;
